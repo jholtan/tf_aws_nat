8bbd1576faf7289f48f1d2d5b841806c10558e92
Jarle Holtan <jarle.holtan@bekk.no>
2016-11-02 12:59:17 +0100

    Fix string escaping

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ resource "aws_instance" "nat" {
     source_dest_check = false
     iam_instance_profile = "${aws_iam_instance_profile.nat_profile.id}"
     key_name = "${var.aws_key_name}"
-    subnet_id = "${element(split(\",\", var.subnet_ids), count.index)}"
-    security_groups = ["${split(\",\", var.security_groups)}"]
+    subnet_id = "${element(split(",", var.subnet_ids), count.index)}"
+    security_groups = ["${split(",", var.security_groups)}"]
     tags {
-        Name = "NAT ${element(split(\",\", var.az_list), count.index)}${count.index+1}"
+        Name = "NAT ${element(split(",", var.az_list), count.index)}${count.index+1}"
     }
-    user_data = "${replace(replace(file(\"${path.module}/nat.conf\"), \"__NETWORKPREFIX__\", \"${var.networkprefix}\"), \"__MYAZ__\", element(split(\",\", var.az_list), count.index))}"
+    user_data = "${replace(replace(file("${path.module}/nat.conf"), "__NETWORKPREFIX__", "${var.networkprefix}"), "__MYAZ__", element(split(",", var.az_list), count.index))}"
     provisioner "remote-exec" {
         inline = [
           "while sudo pkill -0 cloud-init; do sleep 2; done"
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "private_ips" {
-  value = "${join(\",\", aws_instance.nat.*.private_ip)}"
+  value = "${join(",", aws_instance.nat.*.private_ip)}"
 }
 output "public_ips" {
-  value = "${join(\",\", aws_instance.nat.*.public_ip)}"
+  value = "${join(",", aws_instance.nat.*.public_ip)}"
 }
 output "instance_ids" {
-  value = "${join(\",\", aws_instance.nat.*.id)}"
+  value = "${join(",", aws_instance.nat.*.id)}"
 }
